4376893e84335b743d0f94e943d3182c32be9a95
fhluo <20843637+fhluo@users.noreply.github.com>
2026-02-21 17:53:26 +0800

    feat: add `data_path` to `Config`

--- crates/xpic-app/src/config.rs
+++ crates/xpic-app/src/config.rs
@@ -1,18 +1,22 @@
 use gpui::Global;
 use sha2::{Digest, Sha256};
 use std::path::PathBuf;
+use xpic::bing::Market;
 
 pub struct Config {
     pub cache_dir: PathBuf,
+    pub data_dir: PathBuf,
 }
 
 impl Default for Config {
     fn default() -> Self {
+        let base = dirs::data_local_dir()
+            .unwrap_or_else(|| std::env::temp_dir())
+            .join("Xpic");
+
         Self {
-            cache_dir: dirs::cache_dir()
-                .unwrap_or_else(|| std::env::temp_dir())
-                .join("Xpic")
-                .join("cache"),
+            cache_dir: base.join("cache"),
+            data_dir: base.join("data"),
         }
     }
 }
@@ -23,5 +27,9 @@ impl Config {
     pub fn image_cache(&self, url: impl AsRef<str>) -> PathBuf {
         let hash = Sha256::digest(url.as_ref().as_bytes());
         self.cache_dir.join(hex::encode(hash))
+    }
+
+    pub fn data_path(&self, market: Market) -> PathBuf {
+        self.data_dir.join(format!("{}.json", market.code()))
     }
 }
